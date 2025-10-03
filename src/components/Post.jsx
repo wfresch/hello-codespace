5fa836a5a215ecaab9864a8e78eb1b283ddacf21
Will Fresch <wfresch@gmail.com>
2025-10-03 22:27:20 +0000

    chore: commit module 8 changes

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,14 +1,23 @@
 import PropTypes from 'prop-types'
 import { User } from './User.jsx'
-export function Post({ title, contents, author: userId }) {
+import { Link } from 'react-router-dom'
+import slug from 'slug'
+
+export function Post({ title, contents, author, _id, fullPost = false }) {
   return (
     <article>
-      <h3>{title}</h3>
-      <div>{contents}</div>
-      {userId && (
+      {fullPost ? (
+        <h3>{title}</h3>
+      ) : (
+        <Link to={`/posts/${_id}/${slug(title)}`}>
+          <h3>{title}</h3>
+        </Link>
+      )}
+      {fullPost && <div>{contents}</div>}
+      {author && (
         <em>
-          <br />
-          Written by <User id={userId} />
+          {fullPost && <br />}
+          Written by <User id={author} />
         </em>
       )}
     </article>
@@ -18,4 +27,6 @@ Post.propTypes = {
   title: PropTypes.string.isRequired,
   contents: PropTypes.string,
   author: PropTypes.string,
+  _id: PropTypes.string.isRequired,
+  fullPost: PropTypes.bool,
 }
